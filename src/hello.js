48df99c73e4f156c6d46414d5b3bb1525b55fe76
Pascal Prins <pascal.prins@foobar-it.com>
2018-05-16 16:13:47 +0200

    Fixed lint issues + added install npm@latest

--- src/hello.js
+++ src/hello.js
@@ -1,31 +1,29 @@
 exports.handler = (event, context, callback) => {
-
   console.log('-> Received event: ', JSON.stringify(event, null, 2));
 
   let json;
 
   try {
     json = JSON.parse(event.body);
-
   } catch (e) {
-    console.log("Got invalid JSON: ", e);
+    console.log('Got invalid JSON: ', e);
 
     callback(null, {
       statusCode: 400,
       body: JSON.stringify({
-        message: "Body contains invalid JSON."
-      })
-    })
+        message: 'Body contains invalid JSON.',
+      }),
+    });
   }
 
   if (Object.keys(json).length === 0 && json.constructor === Object) {
     callback(null, {
       statusCode: 409,
       body: JSON.stringify({
-        message: "Empty request not allowed."
-      })
-    })
-  };
+        message: 'Empty request not allowed.',
+      }),
+    });
+  }
 
   const response = {
     statusCode: 200,
@@ -39,4 +37,4 @@ exports.handler = (event, context, callback) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-}
+};
